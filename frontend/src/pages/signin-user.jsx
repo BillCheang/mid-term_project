1bdfdf3e071244aa30c4d3f553d09477b80353f9
BillCheang <billcheang20010723ntu@gmail.com>
2024-04-10 17:00:50 +0800

    second commit2

---
+++ frontend/src/pages/signin-user.jsx
@@ -0,0 +1,87 @@
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { useState } from "react";
+//import services from "../services";
+
+function SingnInPage() {
+  const [formData, setFormData] = useState({ username: "", password: "", profilePicture: null });
+  const [message, setMessage] = useState("");
+  const [fileError, setFileError] = useState("");
+
+  const handleTextInputChange = ({ target: { name, value } }) => {
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    //todo.....
+      setFormData({ username: "", password: "", profilePicture: null });
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Sign In
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={handleFormSubmit}>
+            <div className="-space-y-px rounded-md shadow-sm">
+              <div>
+                <label htmlFor="username" className="sr-only">
+                  Username
+                </label>
+                <input
+                  name="username"
+                  type="text"
+                  required
+                  className="relative block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Username"
+                  value={formData.username}
+                  onChange={handleTextInputChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  name="password"
+                  type="password"
+                  required
+                  className="relative block w-full rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  placeholder="Password"
+                  value={formData.password}
+                  onChange={handleTextInputChange}
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                Sign In
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <pre>{message}</pre>
+    </>
+  );
+}
+
+export default SingnInPage;
